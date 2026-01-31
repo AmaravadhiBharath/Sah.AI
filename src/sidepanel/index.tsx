73e15c38002ffc07be4f74fd7682adf644b48ff8
Bharath <amaravadhibharath@gmail.com>
2026-02-01 00:13:09 +0530

    Switch entry point back to AshokApp

--- src/sidepanel/index.tsx
+++ src/sidepanel/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SeviApp from './SeviApp';
+import AshokApp from './AshokApp';
 import { ErrorBoundary } from './ErrorBoundary';
 import '../styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <SeviApp />
+      <AshokApp />
     </ErrorBoundary>
   </React.StrictMode>
 );
